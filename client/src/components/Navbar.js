c6ca92ffd20d66ae95e18b88cbf6245aae32d6ea
Dharmi26 <dharmipatel567@gmail.com>
2023-09-30 22:45:04 +0530

    responsive navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.jpg';
 import { Link } from 'react-router-dom';
-import {BiSearch,BiUserCircle} from 'react-icons/bi';
+import {BiSearch} from 'react-icons/bi';
 import {FaUserCircle} from 'react-icons/fa';
+import {HiBars3} from 'react-icons/hi2';
+import {RxCross1} from 'react-icons/rx';
 const Navbar = () => {
 
     const links = [
@@ -23,9 +25,10 @@ const Navbar = () => {
             link:"Contact",
         },
        ];
+       const [nav,setnav]=useState(false);
   return (
     <div>
-        <nav className="bg-navblue text-white px-2 h-20 flex flex-row justify-between items-center">
+        <nav className="bg-navblue text-white w-full px-2 h-20 flex flex-row justify-between items-center fixed z-0">
             <div className='flex flex-row items-center'>
                 <div className='cursor-pointer'>
                     <img src={logo} alt="EmpowerYouth" width="75"/>
@@ -49,11 +52,38 @@ const Navbar = () => {
                     type="text"
                     placeholder="Search"/>
                 </div>
-                <div className='cursor-pointer'>
+                <div className='hidden md:flex cursor-pointer'>
                     <FaUserCircle size="35"/>
+                </div>
+                <div className='md:hidden cursor-pointer' onClick={()=>setnav(!nav)}>
+                    {nav?(<RxCross1 size="30"/>):(<HiBars3 size="35"/>)}
                 </div>
             </div>
         </nav>
+
+        {nav&&(
+            <div className='md:hidden bg-navblue text-white h-screen w-full flex flex-col align-middle pt-24 z-10'>
+                {/* <div className='flex flex-row justify-center align-center py-3'> */}
+                    <div className='cursor-pointer flex flex-row gap-3 justify-center pb-5'>
+                        <FaUserCircle size="20"/>
+                        <p>Login</p>
+                    </div>
+                    <hr/>
+                {/* </div>  */}
+                <div className='flex flex-col justify-center pt-5'>
+                    <ul className='flex flex-col gap-6 justify-center text-center'>
+                            {links.map((link)=>(
+                                <li
+                                key={link.id} 
+                                className='px-4 cursor-pointer capitalize'
+                                >
+                                <a href="#" smooth duration={500}>{link.link}</a>
+                                </li>
+                            ))}
+                    </ul>
+                </div> 
+            </div>
+            )}
     </div>
   )
 }
