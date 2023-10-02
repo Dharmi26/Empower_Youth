55b6de89d6e085fdc72bbfd24bdef3b173f117e0
Dharmi26 <dharmipatel567@gmail.com>
2023-10-02 19:23:02 +0530

    a

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
       }, []);
   return (
     <div className='min-h-screen bg-bg1 bg-fixed mb-36'>
-        <div className='bg-blue-100 bg-opacity-50 h-full flex flex-col gap-10 justify-center items-center px-48 py-28'>
+        <div className='bg-blue-100 bg-opacity-50 h-full flex flex-col gap-10 justify-center items-center px-6 py-12 sm:px-12 md:px-36 lg:px-48 sm:py-20 md:py-24 lg:py-28'>
             <h1 className='text-navblue text-4xl font-bold'>About</h1>
             <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600" className='text-lg text-justify text-navblue'>
                 In a world where a million youth enter the job market every month, empowering them to make informed career choices in the vocational sector is a critical imperative. The vocational pathway, often overshadowed by traditional academics, holds immense potential for employment and economic growth. However, the lack of accessible and reliable information has resulted in information asymmetry, leaving many young individuals in the dark when it comes to choosing the right vocational trade for their future.
