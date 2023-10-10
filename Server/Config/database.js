e3bd9c3be4273b094d190235785f8a228ef1006b
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-11 00:13:34 +0530

    Create database.js

---
+++ Server/Config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async (req , res) => {
+	try {
+		await mongoose.connect(process.env.MONGOOSE_URL);
+		console.log("Connection to mogoose database is successfull");
+	} 
+	catch(error) {
+		console.log("Something went wrong while connecting to the mongoose database");
+	}
+}
+module.exports = connectToDB;
