a6ea471bfbc7a75d35299522f40e2bf6e5057e0d
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-21 22:20:02 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectToDB = require("./Config/database"); 
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectToDB = require('./Config/database'); 
+const authRoute = require('./Routes/authRouter');
 dotenv.config();
 connectToDB();
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
 
-// authentication microservice
-app.use("/auth" , require("./Routes/authRouter"));
+// authentication microservices
+app.use('/auth' , authRoute);
 
 const port = process.env.port || 8080;
 app.listen(port , () => {
-	console.log("Server is listening on http://localhost:" + port);
+	console.log('Server is listening on http://localhost:' + port);
 });
