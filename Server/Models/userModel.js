70e754d5b09f6c4102ff358baffd153daa3dc347
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-21 22:25:29 +0530

    email registration validation

--- Server/Models/userModel.js
+++ Server/Models/userModel.js
@@ -4,36 +4,32 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
 	name : {
 		type : String,
+		trim : true,
 		required : true
 	},
 	email : {
 		type : String,
+		trim : true,
 		required : true,
 		unique : true,
 	},
-	contactNumber : {
-		type : String,
-		require : true,
-		unique : true,
-		validate : {
-			validator : function(v) {
-				const contactNumRegex = /^[0-9]{10}$/;
-				return contactNumRegex.test(v);
-			}
-		},
-		message : "invalid contact number. Please provide a 10 digit number without country code"
-	},
 	password : {
 		type : String,
+		trim : true,
 		require : true
-	}
+	},
+	tokens : [{type : Object}]
 } , {timestamps : true});
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-	return await bcrypt.compare(enteredPassword , this.password);
+	try {
+		return await bcrypt.compare(enteredPassword , this.password);
+	} catch (error) {
+		console.log('Error occurred while matching passwords : ' + error.message);
+	}
 };
 
-userSchema.pre("save" , async function(next) {
+userSchema.pre('save' , async function(next) {
 	if (!this.isModified("password")) {
 		next();
 	}
@@ -41,4 +37,19 @@ userSchema.pre("save" , async function(next) {
 	this.password = await bcrypt.hash(this.password , salt);
 });
 
-module.exports = mongoose.model("users" , userSchema);
+userSchema.statics.isEmailRegistered = async function (email) {
+	if (!email) {
+		throw new Error('Invalid email');
+	}
+	try {
+		const newUser = this.findOne({email});
+		if (newUser) {
+			return true;
+		}
+		return false;
+	} catch (error) {
+		console.log('Error occurred in the isEmailRegistered method : ' + error.message);
+	}
+}
+
+module.exports = mongoose.model('User' , userSchema);
