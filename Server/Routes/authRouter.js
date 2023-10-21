9396a0dd866902659aa98d6e7a9170f3084ab2c3
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-21 22:30:12 +0530

    logout end-point created

--- Server/Routes/authRouter.js
+++ Server/Routes/authRouter.js
@@ -1,7 +1,33 @@
-const router = require("express").Router();
-const {registerController , loginController} = require("../Controller/authController");
+const express = require('express');
+const router = express.Router();
+const {
+	registerController,
+	loginController,
+	logoutController
+} = require('../Controllers/authController');
+const {
+	userValidation,
+	registerValidation,
+	loginValidation
+} = require('../Middlewares/Validation/validateUser');
+const isAuth = require('../Middlewares/isAuth');
 
-router.post("/register" , registerController);
-router.post("/login" , loginController);
+// end-point : auth/register
+// API : register
+// http request : POST
+
+router.post('/register' , registerValidation , userValidation , registerController);
+
+// end-point : auth/login
+// API : login
+// http request : POST
+
+router.post('/login' , loginValidation , userValidation , loginController);
+
+// end-point : auth/logout
+// API : logout
+// http request : POST
+
+router.post('/logout' , isAuth , logoutController);
 
 module.exports = router;
