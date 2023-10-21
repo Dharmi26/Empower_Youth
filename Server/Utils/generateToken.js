97c0a05d1aa6f4d9f34648e5a941df24eb7926a8
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-21 22:21:09 +0530

    Delete Server/Utils directory

--- Server/Utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-const JWT = require("jsonwebtoken");
-
-const generateToken = function (id) {
-	return JWT.sign({userId : id} , process.env.JWT_SECRET_KEY , {expiresIn : '1d'});
-};
-module.exports = generateToken;
