580eff3cb818813ef134b1903fa29a750622644a
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-21 22:23:44 +0530

    logout functionality implemented

--- Server/Controller/authController.js
+++ Server/Controller/authController.js
@@ -1,57 +1,114 @@
-const userModel = require("../Models/userModel");
-const generateToken = require("../Utils/generateToken");
+const JWT = require('jsonwebtoken');
+const User = require('../Models/userModel');
 
 const registerController = async (req , res) => {
-	const {name , email , contactNumber , password} = req.body;
-	const userExists = await userModel.findOne({email});
-	if (userExists) {
-		return res.status(400).send({
+	const {name , email , password} = req.body;
+	const isNewUser = await User.isEmailRegistered(email);
+	if (!isNewUser) {
+		return res.status(401).send({
 			success : false,
-			message : "User already registered"
+			message : 'User already registered'
 		});
 	}
-	const newUser = await userModel.create({name , email , contactNumber , password});
+
+	const newUser = await User.create({
+		name,
+		email,
+		password
+	});
 	if (newUser) {
-		return res.status(200).send({
+		return res.status(201).send({
 			success : true,
-			message : "User registered successfully",
-			name : newUser.name,
-			email : newUser.email,
-			contactNumber : newUser.contactNumber,
-			token : generateToken(newUser._id)
+			message : 'User registered successfully',
+			user : newUser
 		});
-	}
+	} 
 	else {
-		return res.status(500).send({
+		return res.status(501).send({
 			success : false,
-			message : "Error in register API"
+			message : 'Error in register API',
+			error : error.message
 		});
 	}
 };
 
 const loginController = async (req , res) => {
 	const {email , password} = req.body;
-	const user = await userModel.findOne({email});
+	const user = await User.findOne({email});
 	if (!user) {
-		return res.status(400).send({
+		return res.status(401).send({
 			success : false,
-			message : "email is not registered"
+			message : 'User not registered'
 		});
 	}
-	else if (user && (await user.matchPassword(password))) {
-		return res.status(200).send({
-			success : true,
-			message : "login successfull",
-			name : user.name,
-			email : user.email,
-			token : generateToken(user._id)
+
+	const isMatch = user.matchPassword(password);
+	if (!isMatch) {
+		return res.status(401).send({
+			success : false,
+			message : 'email / password are not matching'
 		});
 	}
-	else {
-		return res.status(400).send({
+
+	const token = JWT.sign({userId : user._id} , process.env.JWT_SECRET_KEY , {expiresIn : '1d'});
+	
+	let oldTokens = user.token || [];
+	if (oldTokens.length) {
+		oldTokens = oldTokens.filter(tkn => {
+			const timeDiff = (Date.now() - parseInt(tkn.signedAt)) / 1000;
+			if (timeDiff < 86400) {
+				return tkn;
+			}
+		});
+	}
+
+	await User.findByIdAndUpdate(user._id , {
+		tokens : [...oldTokens , {
+			token, 
+			signedAt : Date.now().toString()
+		}]
+	});
+	const info = {
+		name : user.name,
+		email : user.email
+	};
+	return res.status(201).send({
+		success : true,
+		message : 'User logged in successfully',
+		user : info,
+		token
+	});
+};
+
+const logoutController = async (req , res) => { 
+	try {
+		if (req.headers && req.headers.authorization) {
+			const token = req.headers.authorization.split(' ')[1];
+			if (!token) {
+				return res.status(400).send({
+					success : false,
+					message : 'Authorization failed'
+				});
+			}
+			const tokens = req.user.tokens;
+			const recentToken = tokens.filter(t => t.token !== token);
+			await User.findByIdAndUpdate(req.user._id , {tokens : recentToken});
+			return res.status(200).send({
+				success : true,
+				message : 'logout successfull' 
+			});
+		}
+	} catch (error) {
+		console.log('Error while logging out : ' + error.message);
+		return res.status(501).send({
 			success : false,
-			message : "email or password are not matching"
+			message : 'Error in logout API'
 		})
 	}
 };
-module.exports = {registerController , loginController};
+
+module.exports = {
+	registerController,
+	loginController,
+	logoutController
+};
