3a96b847ad4eade346478d2cbf25ba9d38115f54
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-11-05 23:39:33 +0530

    testRoute added for testing

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectToDB = require('./Config/database'); 
-const authRoute = require('./Routes/authRouter');
+const authRoute = require('./Routes/authRoute');
 dotenv.config();
 connectToDB();
 
@@ -10,10 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// authentication microservices
-app.use('/auth' , authRoute);
-
 const port = process.env.port || 8080;
 app.listen(port , () => {
 	console.log('Server is listening on http://localhost:' + port);
 });
+
+
+const testRoute = require('./Routes/testRoute');
+
+app.use('/auth' , authRoute);
+
+app.use('/test' , testRoute);
