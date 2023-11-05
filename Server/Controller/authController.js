2ce17393ed05b1747e9c86fefd3a8dc62bc13b04
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-11-05 23:22:53 +0530

    refactored

--- Server/Controller/authController.js
+++ Server/Controller/authController.js
@@ -1,83 +1,91 @@
 const JWT = require('jsonwebtoken');
-const User = require('../Models/userModel');
+const User = require('../Models/User');
 
-const registerController = async (req , res) => {
-	const {name , email , password} = req.body;
-	const isNewUser = await User.isEmailRegistered(email);
-	if (!isNewUser) {
-		return res.status(401).send({
-			success : false,
-			message : 'User already registered'
+const registerController = async (req , res) => { 
+	try {
+		const {name , email , password , isAdmin = false} = req.body;
+		const userFound = await User.findOne({email : email});
+		if (userFound) {
+			return res.status(401).send({
+				success : false,
+				message : 'User already registered'
+			});
+		}
+		const newUser = await User({
+			name,
+			email,
+			isAdmin,
+			password
 		});
-	}
-
-	const newUser = await User.create({
-		name,
-		email,
-		password
-	});
-	if (newUser) {
+		await newUser.save();
 		return res.status(201).send({
 			success : true,
 			message : 'User registered successfully',
 			user : newUser
 		});
-	} 
-	else {
+	} catch (error) {
 		return res.status(501).send({
 			success : false,
-			message : 'Error in register API',
+			message : 'Error in registerController Public API',
 			error : error.message
 		});
 	}
 };
 
-const loginController = async (req , res) => {
-	const {email , password} = req.body;
-	const user = await User.findOne({email});
-	if (!user) {
-		return res.status(401).send({
+const loginController = async (req , res) => { 
+	try {
+		const {email , password} = req.body;
+		const user = await User.findOne({email});
+		if (!user) {
+			return res.status(401).send({
+				success : false,
+				message : 'User not registered'
+			});
+		}
+
+		const isMatch = await user.matchPassword(password);
+		if (!isMatch) {
+			return res.status(401).send({
+				success : false,
+				message : 'email / password are not matching'
+			});
+		}
+
+		const token = JWT.sign({userId : user._id} , process.env.JWT_SECRET_KEY , {expiresIn : '1d'});
+		
+		let oldTokens = user.tokens || [];
+		if (oldTokens.length) {
+			oldTokens = oldTokens.filter(tkn => {
+				const timeDiff = (Date.now() - parseInt(tkn.signedAt)) / 1000;
+				if (timeDiff < 86400) {
+					return tkn;
+				}
+			});
+		}
+
+		await User.findByIdAndUpdate(user._id , {
+			tokens : [...oldTokens , {
+				token, 
+				signedAt : Date.now().toString()
+			}]
+		});
+		const info = {
+			name : user.name,
+			email : user.email
+		};
+		return res.status(201).send({
+			success : true,
+			message : 'User logged in successfully',
+			user : info,
+			token
+		});
+	} catch (error) {
+		return res.status(500).send({
 			success : false,
-			message : 'User not registered'
+			message : 'Error in loginController Public API',
+			error : error.message
 		});
 	}
-
-	const isMatch = user.matchPassword(password);
-	if (!isMatch) {
-		return res.status(401).send({
-			success : false,
-			message : 'email / password are not matching'
-		});
-	}
-
-	const token = JWT.sign({userId : user._id} , process.env.JWT_SECRET_KEY , {expiresIn : '1d'});
-	
-	let oldTokens = user.tokens || [];
-	if (oldTokens.length) {
-		oldTokens = oldTokens.filter(tkn => {
-			const timeDiff = (Date.now() - parseInt(tkn.signedAt)) / 1000;
-			if (timeDiff < 86400) {
-				return tkn;
-			}
-		});
-	}
-
-	await User.findByIdAndUpdate(user._id , {
-		tokens : [...oldTokens , {
-			token, 
-			signedAt : Date.now().toString()
-		}]
-	});
-	const info = {
-		name : user.name,
-		email : user.email
-	};
-	return res.status(201).send({
-		success : true,
-		message : 'User logged in successfully',
-		user : info,
-		token
-	});
 };
 
 const logoutController = async (req , res) => { 
@@ -102,7 +110,7 @@ const logoutController = async (req , res) => {
 		console.log('Error while logging out : ' + error.message);
 		return res.status(501).send({
 			success : false,
-			message : 'Error in logout API'
+			message : 'Error in logoutController Public API'
 		})
 	}
 };
